ccb2c974e5ee4a95c396dff993cf41b84cd67098
pavlimi2 <pavlimi2@uhk.cz>
2024-03-20 13:41:42 +0100

    Rozšíření třídy hlavní

--- src/main/java/Hlavni.java
+++ src/main/java/Hlavni.java
@@ -5,6 +5,6 @@ Hlavní třída
 public class Hlavni {
     public static void main(String[] args){
         System.out.println("hlavní třída");
-
+        System.out.println("Přidano v klonu");
     }
 }
